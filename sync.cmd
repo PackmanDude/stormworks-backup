8ac7cfe97bc9835b4cb1bf0c49d90e29476e6035
Mr. Packman <99031872+PackmanDude@users.noreply.github.com>
2022-03-22 01:29:18 +0200

    Update sync.cmd

--- sync.cmd
+++ sync.cmd
@@ -3,6 +3,6 @@ cd %appdata%\Stormworks\data
 call git clone https://github.com/PackmanDude/stormworks-backup
 cd %appdata%\Stormworks\data\stormworks-backup
 copy %appdata%\Stormworks\data\vehicles
-call git add .
-call git commit -m "Automatic update"
+call git add *
+call git commit -m "Automatic upload"
 call git push
