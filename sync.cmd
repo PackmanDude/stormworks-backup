f3cd9409af8cf006297c8c312d0022e3646cfdf9
Mr. Packman <99031872+PackmanDude@users.noreply.github.com>
2022-11-08 20:29:42 +0200

    Delete sync.cmd

--- sync.cmd
+++
@@ -1,9 +0,0 @@
-@echo on
-cd %appdata%\Stormworks\data
-call git clone https://github.com/PackmanDude/stormworks-backup
-cd .\stormworks-backup
-copy %appdata%\Stormworks\data\vehicles
-copy %appdata%\Stormworks\data\microprocessors
-call git add *
-call git commit -m "Automatic upload"
-call git push
